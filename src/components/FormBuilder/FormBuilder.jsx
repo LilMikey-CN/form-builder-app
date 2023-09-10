112a0e456f7d91ec1634bc415c90f5fa5d533506
Xingkai Jiao <31362189+LilMikey-CN@users.noreply.github.com>
2023-09-11 03:36:23 +1000

    form editor added feature: question type

--- src/components/FormBuilder/FormBuilder.jsx
+++ src/components/FormBuilder/FormBuilder.jsx
@@ -1,50 +1,72 @@
 import * as React from 'react';
 import axios from 'axios';
-import { Button, TextField, Typography, Container } from '@mui/material';
+import { Button, TextField, Typography, Container, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
 import { styled } from '@mui/system';
 import { API_BASE_URL, FORMS_ENDPOINT } from '../../apiConfig';
 
+const QuestionContainer = styled('div')({
+  marginBottom: '16px',
+  paddingLeft: '8px',
+  borderLeft: '4px solid #3f51b5',
+});
 
-const StyledTextField = styled(TextField)`
-  margin: 8px;
-`;
-
-const StyledButton = styled(Button)`
-  margin: 8px;
-`;
+const OptionContainer = styled('div')({
+  marginBottom: '8px',
+  paddingLeft: '24px',
+  borderLeft: '4px solid #f50057',
+});
 
 const FormBuilder = () => {
   const [form, setForm] = React.useState({ title: '', questions: [] });
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setForm({ ...form, [name]: value });
+  const handleAddQuestion = () => {
+    setForm({
+      ...form,
+      questions: [
+        ...form.questions,
+        { type: 'short_answer', text: '', options: [] },
+      ],
+    });
   };
 
-  const addQuestion = () => {
-    setForm({ ...form, questions: [...form.questions, { title: '', options: [''] }] });
-  };
-
-  const handleQuestionChange = (index, e) => {
+  const handleQuestionChange = (index, value) => {
     const newQuestions = [...form.questions];
-    newQuestions[index].title = e.target.value;
+    newQuestions[index].text = value;
     setForm({ ...form, questions: newQuestions });
   };
 
-  const handleOptionChange = (questionIndex, optionIndex, e) => {
+  const handleAddOption = (questionIndex) => {
     const newQuestions = [...form.questions];
-    newQuestions[questionIndex].options[optionIndex] = e.target.value;
+    newQuestions[questionIndex].options.push('');
     setForm({ ...form, questions: newQuestions });
   };
 
-  const addOption = (index) => {
+  const handleOptionChange = (questionIndex, optionIndex, value) => {
     const newQuestions = [...form.questions];
-    newQuestions[index].options.push('');
+    newQuestions[questionIndex].options[optionIndex] = value;
+    setForm({ ...form, questions: newQuestions });
+  };
+
+  const handleQuestionTypeChange = (questionIndex, value) => {
+    const newQuestions = [...form.questions];
+    newQuestions[questionIndex].type = value;
+    setForm({ ...form, questions: newQuestions });
+  };
+
+  const handleDeleteQuestion = (questionIndex) => {
+    const newQuestions = form.questions.filter((_, index) => index !== questionIndex);
+    setForm({ ...form, questions: newQuestions });
+  };
+
+  const handleDeleteOption = (questionIndex, optionIndex) => {
+    const newQuestions = [...form.questions];
+    newQuestions[questionIndex].options = newQuestions[questionIndex].options.filter((_, index) => index !== optionIndex);
     setForm({ ...form, questions: newQuestions });
   };
 
   const handleSubmit = async () => {
     try {
+      console.log('Submitting form:', form);
       await axios.post(`${API_BASE_URL}${FORMS_ENDPOINT}`, form);
       alert('Form submitted successfully');
     } catch (error) {
@@ -54,49 +76,61 @@ const FormBuilder = () => {
 
   return (
     <Container>
-      <Typography variant="h4" gutterBottom>
-        Create Form
-      </Typography>
-      <form noValidate autoComplete="off">
-        <StyledTextField
-          label="Form Title"
-          variant="outlined"
-          name="title"
-          fullWidth
-          value={form.title}
-          onChange={handleInputChange}
-        />
-        {form.questions.map((question, qIndex) => (
-          <div key={qIndex}>
-            <StyledTextField
-              label="Question Title"
-              variant="outlined"
-              fullWidth
-              value={question.title}
-              onChange={(e) => handleQuestionChange(qIndex, e)}
-            />
-            {question.options.map((option, oIndex) => (
-              <StyledTextField
-                key={oIndex}
-                label="Option"
-                variant="outlined"
-                fullWidth
-                value={option}
-                onChange={(e) => handleOptionChange(qIndex, oIndex, e)}
-              />
-            ))}
-            <StyledButton variant="contained" onClick={() => addOption(qIndex)}>
-              Add Option
-            </StyledButton>
-          </div>
-        ))}
-        <StyledButton variant="contained" onClick={addQuestion}>
-          Add Question
-        </StyledButton>
-        <StyledButton variant="contained" color="primary" onClick={handleSubmit}>
-          Create Form
-        </StyledButton>
-      </form>
+      <Typography variant="h4" mb={2}>Form Builder</Typography>
+      <TextField
+        label="Form Title"
+        fullWidth
+        variant="outlined"
+        mb={3}
+        value={form.title}
+        onChange={(e) => setForm({ ...form, title: e.target.value })}
+      />
+      {form.questions.map((question, questionIndex) => (
+        <QuestionContainer key={questionIndex}>
+          <FormControl variant="outlined" fullWidth mb={2}>
+            <InputLabel>Question Type</InputLabel>
+            <Select
+              value={question.type}
+              onChange={(e) => handleQuestionTypeChange(questionIndex, e.target.value)}
+              label="Question Type"
+            >
+              <MenuItem value="short_answer">Short Answer</MenuItem>
+              <MenuItem value="paragraph">Paragraph</MenuItem>
+              <MenuItem value="multiple_choice">Multiple Choice</MenuItem>
+              <MenuItem value="checkbox">Checkbox</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            label="Question Text"
+            fullWidth
+            variant="outlined"
+            mb={2}
+            value={question.text}
+            onChange={(e) => handleQuestionChange(questionIndex, e.target.value)}
+          />
+          {(question.type === 'multiple_choice' || question.type === 'checkbox') && (
+            <>
+              {question.options.map((option, optionIndex) => (
+                <OptionContainer key={optionIndex}>
+                  <TextField
+                    label="Option"
+                    fullWidth
+                    variant="outlined"
+                    mb={2}
+                    value={option}
+                    onChange={(e) => handleOptionChange(questionIndex, optionIndex, e.target.value)}
+                  />
+                  <Button variant="contained" color="error" onClick={() => handleDeleteOption(questionIndex, optionIndex)}>Delete Option</Button>
+                </OptionContainer>
+              ))}
+              <Button variant="contained" onClick={() => handleAddOption(questionIndex)}>Add Option</Button>
+            </>
+          )}
+          <Button variant="contained" color="error" onClick={() => handleDeleteQuestion(questionIndex)}>Delete Question</Button>
+        </QuestionContainer>
+      ))}
+      <Button variant="contained" onClick={handleAddQuestion}>Add Question</Button>
+      <Button variant="contained" color="primary" onClick={handleSubmit}>Submit Form</Button>
     </Container>
   );
 };
